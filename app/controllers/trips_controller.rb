21e2f0f949479211e91c0a3a1c613d80109679de
Raghu Betina <raghu@firstdraft.com>
2019-06-05 16:32:08 +0000

    Generate trips resource with draft:resource

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,63 @@
+class TripsController < ApplicationController
+  def index
+    @trips = Trip.all
+
+    render("trip_templates/index.html.erb")
+  end
+
+  def show
+    @trip = Trip.find(params.fetch("id_to_display"))
+
+    render("trip_templates/show.html.erb")
+  end
+
+  def new_form
+    @trip = Trip.new
+
+    render("trip_templates/new_form.html.erb")
+  end
+
+  def create_row
+    @trip = Trip.new
+
+    @trip.name = params.fetch("name")
+    @trip.user_id = params.fetch("user_id")
+
+    if @trip.valid?
+      @trip.save
+
+      redirect_back(:fallback_location => "/trips", :notice => "Trip created successfully.")
+    else
+      render("trip_templates/new_form_with_errors.html.erb")
+    end
+  end
+
+  def edit_form
+    @trip = Trip.find(params.fetch("prefill_with_id"))
+
+    render("trip_templates/edit_form.html.erb")
+  end
+
+  def update_row
+    @trip = Trip.find(params.fetch("id_to_modify"))
+
+    @trip.name = params.fetch("name")
+    @trip.user_id = params.fetch("user_id")
+
+    if @trip.valid?
+      @trip.save
+
+      redirect_to("/trips/#{@trip.id}", :notice => "Trip updated successfully.")
+    else
+      render("trip_templates/edit_form_with_errors.html.erb")
+    end
+  end
+
+  def destroy_row
+    @trip = Trip.find(params.fetch("id_to_remove"))
+
+    @trip.destroy
+
+    redirect_to("/trips", :notice => "Trip deleted successfully.")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Trip resource:
+
+  # CREATE
+  get("/trips/new", { :controller => "trips", :action => "new_form" })
+  post("/create_trip", { :controller => "trips", :action => "create_row" })
+
+  # READ
+  get("/trips", { :controller => "trips", :action => "index" })
+  get("/trips/:id_to_display", { :controller => "trips", :action => "show" })
+
+  # UPDATE
+  get("/trips/:prefill_with_id/edit", { :controller => "trips", :action => "edit_form" })
+  post("/update_trip/:id_to_modify", { :controller => "trips", :action => "update_row" })
+
+  # DELETE
+  get("/delete_trip/:id_to_remove", { :controller => "trips", :action => "destroy_row" })
+
+  #------------------------------
+
   devise_for :users
   # Routes for the User resource:
 
