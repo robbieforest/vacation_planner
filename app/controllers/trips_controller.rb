61f07a880c9f47917f8b9b69937fb3b5b911e0b1
Raghu Betina <raghu@firstdraft.com>
2019-06-05 16:33:07 +0000

    Only trip user can modify trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,14 @@
 class TripsController < ApplicationController
+  before_action :current_user_must_be_trip_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_trip_user
+    trip = Trip.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == trip.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @trips = Trip.all
 
